aab97cbbb4b5d3589ef83bd08136f15aff3f2260
Timo Hermans <timo.hermans@kabisa.nl>
2021-12-30 16:13:43 +0100

    Only use page in onMount
    see https://github.com/sveltejs/kit/pull/2910/files

--- src/lib/MonthPicker.svelte
+++ src/lib/MonthPicker.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { date } from '$lib/store';
+	import { onMount } from 'svelte';
 
-	$: year = +$page.query.get('year') || new Date().getFullYear();
-	$: month = $page.query.has('month') ? +$page.query.get('month') - 1 : new Date().getMonth();
-	$: selectedDate = new Date(year, month, 1);
+	let selectedDate = new Date();
 
-	$: {
-		console.log(year);
-		console.log(month);
-		console.log(selectedDate);
+	onMount(() => {
+		const year = +$page.query.get('year') || new Date().getFullYear();
+		const month = $page.query.has('month') ? +$page.query.get('month') - 1 : new Date().getMonth();
+		selectedDate = new Date(year, month, 1);
+
 		if (!$page.query.has('year') || !$page.query.has('month')) {
 			date.set(new Date());
 		} else {
 			date.set(selectedDate);
 		}
-	}
+	});
 </script>
 
 <h2>
