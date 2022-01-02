404f2d54576f4aa260e48b6bb98b28b9f2bc2c3b
Timo Hermans <timo.hermans@kabisa.nl>
2022-01-02 15:59:37 +0100

    Make it possible to navigate months

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import Authentication from '$lib/Authentication.svelte';
 	import { date } from '$lib/store';
+
+	let previousDate;
+	let nextDate;
+
+	$: if ($date) {
+		updateDates($date);
+	}
+
+	function updateDates(date: Date) {
+		previousDate = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+		nextDate = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+	}
 </script>
 
 <nav>
@@ -12,6 +24,16 @@
 					{$date?.getFullYear()}-{$date?.getMonth() + 1}
 				{/if}
 			</strong>
+		</li>
+		<li>
+			<a
+				rel="external"
+				href="?year={previousDate?.getFullYear()}&month={previousDate?.getMonth() + 1}">←</a
+			>
+		</li>
+		<li>
+			<a rel="external" href="?year={nextDate?.getFullYear()}&month={nextDate?.getMonth() + 1}">→</a
+			>
 		</li>
 	</ul>
 	<ul>
