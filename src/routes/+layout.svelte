fd9225690fcb06c1d0736da7b0fbe3ad717c9cc1
Timo Hermans <tmhermans@gmail.com>
2023-04-04 21:18:02 +0200

    Move picocss locally
    the cdn was out. not chill

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,16 @@
 	import Header from '$lib/Header.svelte';
 	import type { PageServerData } from './$types';
 	import { date } from '$lib/store';
+	import pico from '@picocss/pico/css/pico.min.css';
 
 	export let data: PageServerData;
 
 	date.set(data.date);
 </script>
+
+<svelte:head>
+	<link rel="stylesheet" href={pico}>
+</svelte:head>
 
 <Header user={data.user} date={data.date} />
 <MonthPicker />
