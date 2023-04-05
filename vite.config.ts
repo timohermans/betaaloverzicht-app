8f48d1c50830e9f4b12e7559e718e35c4d657d87
Timo Hermans <tmhermans@gmail.com>
2023-04-05 11:07:54 +0200

    Remove all testing leftovers

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}'],
-		setupFiles: ['src/lib/testUtils/setup.ts'],
-		globals: true,
-		environment: 'jsdom',
-	}
+	plugins: [sveltekit()]
 });
